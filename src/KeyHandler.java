dbecc4e401005d54cf9045993fa494afea70a956
W <ww12c245g@gmail.com>
2024-02-15 19:15:37 +0100

    1

---
+++ src/KeyHandler.java
@@ -0,0 +1,50 @@
+package src;
+
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+public class KeyHandler implements KeyListener {
+
+    Player player;
+
+    public KeyHandler(Player player){
+        this.player = player;
+    }
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        if(e.getKeyCode()==KeyEvent.VK_UP) {
+            player.keys[0] = true;
+        }
+        if(e.getKeyCode()==KeyEvent.VK_DOWN) {
+            player.keys[1] = true;
+        }
+        if(e.getKeyCode()==KeyEvent.VK_RIGHT) {
+            player.keys[2] = true;
+        }
+        if(e.getKeyCode()==KeyEvent.VK_LEFT) {
+            player.keys[3] = true;
+        }
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+        if(e.getKeyCode()==KeyEvent.VK_UP) {
+            player.keys[0] = false;
+        }
+        if(e.getKeyCode()==KeyEvent.VK_DOWN) {
+            player.keys[1] = false;
+        }
+        if(e.getKeyCode()==KeyEvent.VK_RIGHT) {
+            player.keys[2] = false;
+        }
+        if(e.getKeyCode()==KeyEvent.VK_LEFT) {
+            player.keys[3] = false;
+        }
+    }
+}
---
+++ src/MainRenderer.java
@@ -0,0 +1,32 @@
+package src;
+
+import maths.Vector2;
+import maths.Vector3;
+import renderer.*;
+import util.Console;
+
+import java.awt.*;
+import java.io.File;
+
+public class MainRenderer extends Renderer {
+
+    public Triangles triangles;
+    public Model torus;
+    String classPath = getClass().getResource("torus.model").getPath(); // żebyśmy nie musieli tego pisać za każdym razem
+     public MainRenderer(Vector2 dimensions, Camera camera) {
+            super(dimensions, camera);
+            triangles = new Triangles();
+            try {
+                torus = LoadModel.loadModel(new File("C:\\Users\\Wiktoria Mazur\\Desktop\\Asteroids\\src\\torus.model"), Color.green, camera.renderer, camera); // ładujemy model z pliku
+                torus.init(triangles); // inicjalizujemy model (wymagane)
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            KeyHandler keyHandler = new KeyHandler(new Player(torus, new Vector3(0,0,0),));
+     }
+
+     public void render(Graphics2D graphics) {
+         triangles.render(graphics);
+     }
+
+}
