f807430e79e2f920d89bed27bdd4e3f59ad7f562
bwegrzyn0 <bwegrzyn@proton.me>
2024-02-21 15:58:24 +0100

    mechanika ruchu

--- src/Player.java
+++ src/Player.java
@@ -8,16 +8,17 @@ import util.Console;
 
 public class Player extends Entity {//
     boolean keys[] = new boolean[4];
+    boolean moving = false;
     public Player(Model model, Vector3 vector3, EntityHandler entityHandler) {
         super(model, vector3, entityHandler);
     }
 
     double theta = 0;
-    double s = 0.01;
+    double s = 0.00025;
+    double maxs = 0.01;
 
     public void logic() {
-        velocity.x = 0;
-        velocity.y = 0;
+	moving = false;
         if(keys[2]){
             theta += 0.1;
             model.rotate(2,-0.1);
@@ -27,22 +28,18 @@ public class Player extends Entity {//
             model.rotate(2,0.1);
         }
 
-        if(keys[0]){ //w gore
-           velocity.y =  s * Math.cos(theta);
-           velocity.x =  s * Math.sin(theta);
+        if(keys[0]){ // w gore
+           velocity.y +=  s * Math.cos(theta);
+           velocity.x +=  s * Math.sin(theta);
+	   moving = true;
         }
-        else if(keys[1]){
-            velocity.y = -s * Math.cos(theta)/2;
-            velocity.x = -s * Math.sin(theta)/2;
+        else if(keys[1]){ // w dol
+	    double c = 1-velocity.magnitude()/maxs;
+            velocity.y += -s * Math.cos(theta)/2*c;
+            velocity.x += -s * Math.sin(theta)/2*c;
+	    moving = true;
         }
-        model.updateVerticies();
         position.add(velocity);
         model.move(velocity);
-
-
-
-
-
-
     }
 }
