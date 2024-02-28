902849c08acef5269ee6d127f9653216448e33bc
bwegrzyn0 <bwegrzyn@proton.me>
2024-02-28 09:59:49 +0100

    prosty hud

---
+++ src/HUD.java
@@ -0,0 +1,27 @@
+package src;
+
+import java.awt.*;
+import util.Console;
+
+public class HUD {
+
+	public int score;
+	public int lifes;
+	Player player;
+	Font font;
+	public HUD(Player player) {
+		this.player = player;	
+		font = new Font("Sans Serif", Font.PLAIN, 30);
+	}
+	
+	public void update() {
+
+	}
+
+	public void render(Graphics2D graphics) {
+		graphics.setColor(Color.white);
+		graphics.setFont(font);
+		graphics.drawString("Score: " + score, 10, 30);
+		graphics.drawString("Lifes: " + lifes, 10, 60);
+	}
+}
