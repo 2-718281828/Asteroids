fcb96632130ce704056f937073288bcb0f10bc6d
bwegrzyn0 <bwegrzyn@duck.com>
2024-02-29 21:10:16 +0100

    ufo, etapy, zycie za pkt

--- src/Bullet.java
+++ src/Bullet.java
@@ -7,70 +7,79 @@ import renderer.Model;
 
 public class Bullet extends Entity {
 
-    MainRenderer renderer;
-    Vector3 invPosition = new Vector3(0, 0, 0);
+	MainRenderer renderer;
+	Vector3 invPosition = new Vector3(0, 0, 0);
 
 
-    public Bullet(Model bulletM, Vector3 vector3, EntityHandler entityHandler, Vector3 velocity, MainRenderer renderer) {
-        super(bulletM, vector3, entityHandler);
-        this.velocity = velocity;
-        this.renderer = renderer;
-        model.scale(0.005);
-    }
+	public Bullet(Model bulletM, Vector3 vector3, EntityHandler entityHandler, Vector3 velocity, MainRenderer renderer) {
+		super(bulletM, vector3, entityHandler);
+		this.velocity = velocity;
+		this.renderer = renderer;
+		model.scale(0.005);
+	}
 
-    int t = 0;
+	int t = 0;
 
-    @Override
-    public void logic() {
+	@Override
+	public void logic() {
 
-        // odmierza czas aby usunąć ten obiekt
-        t++;
-        if (t >= 70) {
-            model.remove(renderer.triangles);
-            entityHandler.entities.remove(this);
-        }
-
-
-        invPosition.x = position.x;
-        invPosition.y = position.y;
-        if (position.x <= -1) {
-            position.x = 1;
-        } else if (position.x >= 1) {
-            position.x = -1;
-        }
-        if (position.y <= -1 * (renderer.dimensions.y / renderer.dimensions.x)) {
-            position.y = 1 * (renderer.dimensions.y / renderer.dimensions.x);
-        } else if (position.y >= 1 * (renderer.dimensions.y / renderer.dimensions.x)) {
-            position.y = -1 * (renderer.dimensions.y / renderer.dimensions.x);
-        }
-
-        invPosition.multiply(-1);
-        invPosition.add(position);
-        model.move(invPosition);
-
-        // przemieszcza obiekt i model
-        position.add(velocity);
-        model.move(velocity);
-
-        for (int i = 0; i < entityHandler.entities.size(); i++) {
-            if (entityHandler.entities.get(i) != this) {
-                if (collision(entityHandler.entities.get(i).hitbox)) {
-                    if (entityHandler.entities.get(i).getClass() == Asteroid.class) {
-			renderer.hud.score += (int) (((Asteroid) entityHandler.entities.get(i)).size / 0.7 * 500);
+		// odmierza czas aby usunąć ten obiekt
+		t++;
+		if (t >= 70) {
 			model.remove(renderer.triangles);
-			if (((Asteroid) entityHandler.entities.get(i)).size >= 0.3) {
-				renderer.spawnAsteroid(((Asteroid) entityHandler.entities.get(i)).size/2, false, entityHandler.entities.get(i).position, renderer.triangles, entityHandler);
-				renderer.spawnAsteroid(((Asteroid) entityHandler.entities.get(i)).size/2, false, entityHandler.entities.get(i).position, renderer.triangles, entityHandler);
-			}
-
-                        entityHandler.entities.get(i).model.remove(renderer.triangles);
-                        entityHandler.entities.remove(entityHandler.entities.get(i));
 			entityHandler.entities.remove(this);
-			break;
-                    }
-                }
-            }
-        }
-    }
+		}
+
+
+		invPosition.x = position.x;
+		invPosition.y = position.y;
+		if (position.x <= -1) {
+			position.x = 1;
+		} else if (position.x >= 1) {
+			position.x = -1;
+		}
+		if (position.y <= -1 * (renderer.dimensions.y / renderer.dimensions.x)) {
+			position.y = 1 * (renderer.dimensions.y / renderer.dimensions.x);
+		} else if (position.y >= 1 * (renderer.dimensions.y / renderer.dimensions.x)) {
+			position.y = -1 * (renderer.dimensions.y / renderer.dimensions.x);
+		}
+
+		invPosition.multiply(-1);
+		invPosition.add(position);
+		model.move(invPosition);
+
+		// przemieszcza obiekt i model
+		position.add(velocity);
+		model.move(velocity);
+
+		for (int i = 0; i < entityHandler.entities.size(); i++) {
+			if (entityHandler.entities.get(i) != this) {
+				if (collision(entityHandler.entities.get(i).hitbox)) {
+					if (entityHandler.entities.get(i).getClass() == Asteroid.class) {
+						renderer.hud.score += (int) (((Asteroid) entityHandler.entities.get(i)).size / 0.7 * 500);
+						model.remove(renderer.triangles);
+						if (((Asteroid) entityHandler.entities.get(i)).size >= 0.3) {
+							renderer.spawnAsteroid(((Asteroid) entityHandler.entities.get(i)).size/2, false, entityHandler.entities.get(i).position, renderer.triangles, entityHandler);
+							renderer.spawnAsteroid(((Asteroid) entityHandler.entities.get(i)).size/2, false, entityHandler.entities.get(i).position, renderer.triangles, entityHandler);
+						}
+
+						entityHandler.entities.get(i).model.remove(renderer.triangles);
+						entityHandler.entities.remove(entityHandler.entities.get(i));
+						entityHandler.entities.remove(this);
+						break;
+					}
+					else if (entityHandler.entities.get(i).getClass() == Ufo.class) {
+						renderer.hud.score += 1500;
+						model.remove(renderer.triangles);
+						entityHandler.entities.get(i).model.remove(renderer.triangles);
+						entityHandler.entities.remove(entityHandler.entities.get(i));
+						entityHandler.entities.remove(this);
+						break;
+					}
+
+				}
+			}
+		}
+	}
 
 }
--- src/KeyHandler.java
+++ src/KeyHandler.java
@@ -10,67 +10,66 @@ import java.security.Key;
 
 public class KeyHandler implements KeyListener {
 
-    Player player;
+	Player player;
 
-    public KeyHandler(Player player){
-        //Console.log("a");
-        this.player = player;
-    }
+	public KeyHandler(Player player){
+		this.player = player;
+	}
 
-    @Override
-    public void keyTyped(KeyEvent e) {
+	@Override
+	public void keyTyped(KeyEvent e) {
 
-    }
+	}
 
-    @Override
-    public void keyPressed(KeyEvent e) {
-        if(e.getKeyCode()==KeyEvent.VK_UP) {
-            player.keys[0] = true;
-        }
-        if(e.getKeyCode()==KeyEvent.VK_DOWN) {
-            player.keys[1] = true;
-        }
-        if(e.getKeyCode()==KeyEvent.VK_RIGHT) {
-            player.keys[2] = true;
-        }
-        if(e.getKeyCode()==KeyEvent.VK_LEFT) {
-            player.keys[3] = true;
-        }
-        if(e.getKeyCode()==KeyEvent.VK_SPACE){
-            player.space = true;
-        }
+	@Override
+	public void keyPressed(KeyEvent e) {
+		if(e.getKeyCode()==KeyEvent.VK_UP) {
+			player.keys[0] = true;
+		}
+		if(e.getKeyCode()==KeyEvent.VK_DOWN) {
+			player.keys[1] = true;
+		}
+		if(e.getKeyCode()==KeyEvent.VK_RIGHT) {
+			player.keys[2] = true;
+		}
+		if(e.getKeyCode()==KeyEvent.VK_LEFT) {
+			player.keys[3] = true;
+		}
+		if(e.getKeyCode()==KeyEvent.VK_SPACE){
+			player.space = true;
+		}
 
-    }
+	}
 
-    @Override
-    public void keyReleased(KeyEvent e) {
-        if(e.getKeyCode()==KeyEvent.VK_UP) {
-            player.keys[0] = false;
-        }
-        if(e.getKeyCode()==KeyEvent.VK_DOWN) {
-            player.keys[1] = false;
-        }
-        if(e.getKeyCode()==KeyEvent.VK_RIGHT) {
-            player.keys[2] = false;
-        }
-        if(e.getKeyCode()==KeyEvent.VK_LEFT) {
-            player.keys[3] = false;
-        }
-        if(e.getKeyCode()==KeyEvent.VK_SPACE){
-            player.space = false;
-        }
-        if(!player.renderer.game) {
-            if (e.getKeyCode()==KeyEvent.VK_ENTER){
-                player.renderer.game = true;
-                player.renderer.startGame();
-                try {
-                    Sounds.play("1.wav");
-                }
-                catch (Exception q) {
-                    q.printStackTrace();
-                }
-            }
-        }
+	@Override
+	public void keyReleased(KeyEvent e) {
+		if(e.getKeyCode()==KeyEvent.VK_UP) {
+			player.keys[0] = false;
+		}
+		if(e.getKeyCode()==KeyEvent.VK_DOWN) {
+			player.keys[1] = false;
+		}
+		if(e.getKeyCode()==KeyEvent.VK_RIGHT) {
+			player.keys[2] = false;
+		}
+		if(e.getKeyCode()==KeyEvent.VK_LEFT) {
+			player.keys[3] = false;
+		}
+		if(e.getKeyCode()==KeyEvent.VK_SPACE){
+			player.space = false;
+		}
+		if(!player.renderer.game) {
+			if (e.getKeyCode()==KeyEvent.VK_ENTER){
+				player.renderer.game = true;
+				player.renderer.startGame();
+				try {
+					Sounds.play("1.wav");
+				}
+				catch (Exception q) {
+					q.printStackTrace();
+				}
+			}
+		}
 
-    }
+	}
 }
